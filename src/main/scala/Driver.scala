c5baa59d48edf20777abab1704051f157361e0e5
ajit1865 <125342543+ajit1865@users.noreply.github.com>
2023-03-30 23:36:41 +0530

    Update Driver.scala

--- src/main/scala/Driver.scala
+++ src/main/scala/Driver.scala
@@ -2,7 +2,7 @@ import newRoughWork.{Banktransfer, CreditCard, Paypal}
 
 import scala.io.StdIn
 object PaymentMethodMain extends App {
-  val objOfCreditCard = CreditCard(89768768L, "12/mar/2025", 324)
+  val objOfCreditCard = CreditCard(89768768L, "12/mar", 324)
   val objOfPaypal = Paypal("ajit.kumar@knoldus.com", "Pass@123")
   val objOfBankTransfer = Banktransfer(7865467983L, 1234)
   println(s"Enter a option to add payment \n1. CreditCard\n2. Paypal\n3. AccountTransfer")
